2d6e9ce70709e273a61e8d022e9575f3fbde7560
David King Rhodes <david.king.rhodes@gmail.com>
2023-11-19 19:31:07 +0800

    load env from file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from hosthatch_manager.manager import main
+from dotenv import load_dotenv
+import os.path
 
 if __name__ == "__main__":
+    env_file_path = os.getenv("ENV_FILE_PATH", ".envfile")
+    if os.path.exists(env_file_path):
+        load_dotenv(env_file_path)
     main()
